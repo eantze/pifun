cbdd4ae1e5bcf3d3f8b4e24ce4c02aaea2a57a58
eantze <eric.antze@gmail.com>
2016-05-20 17:41:41 -0700

    .....

--- securitycam.py
+++ securitycam.py
@@ -11,7 +11,7 @@ class MyMotionDetector(picamera.array.PiMotionAnalysis):
             ).clip(0, 255).astype(np.uint8)
         # If there're more than 10 vectors with a magnitude greater
         # than 60, then say we've detected motion
-        if (a > 60).sum() > 25:
+        if (a > 600).sum() > 10:
             for filename in camera.capture_continuous('Spy{counter:03d}.jpg', use_video_port=True):
                 print('Motion detected!')
                 print('Captured %s' % filename)
@@ -26,5 +26,5 @@ with picamera.PiCamera() as camera:
         '/dev/null', format='h264',
         motion_output=MyMotionDetector(camera)
         )
-    camera.wait_recording(3000)
+    camera.wait_recording(300)
     camera.stop_recording()
